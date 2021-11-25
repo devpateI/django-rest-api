6fb08a26d9068ba0eda8a809013c2802a44b5e8f
Dev Patel <dev.dp721@gmail.com>
2021-11-25 12:01:26 +0530

    added api_view

--- basic_api/views.py
+++ basic_api/views.py
@@ -4,48 +4,88 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
-@csrf_exempt
+# @csrf_exempt #only for basic api
+@api_view(['GET', 'POST'])
 def article_list(request):
     """
     List all code articles, or create a new article.
     """
+    # if request.method == 'GET':
+    #     articles = Article.objects.all()
+    #     serializer = ArticleSerializer(articles, many=True)
+    #     return JsonResponse(serializer.data, safe=False)
+
+    # elif request.method == 'POST':
+    #     data = JSONParser().parse(request)
+    #     serializer = ArticleSerializer(data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse(serializer.data, status=201)
+    #     return JsonResponse(serializer.errors, status=400)
+
+    # for Using Django API view
     if request.method == 'GET':
         articles = Article.objects.all()
         serializer = ArticleSerializer(articles, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(data=data)
+        serializer = ArticleSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt
+# @csrf_exempt #only for basic api
+@api_view(['GET', 'PUT', 'DELETE'])
 def article_detail(request, pk):
     """
     Retrieve, update or delete a code article.
     """
+    # try:
+    #     article = Article.objects.get(pk=pk)
+    # except Article.DoesNotExist:
+    #     return HttpResponse(status=404)
+
+    # if request.method == 'GET':
+    #     serializer = ArticleSerializer(article)
+    #     return JsonResponse(serializer.data)
+
+    # elif request.method == 'PUT':
+    #     data = JSONParser().parse(request)
+    #     serializer = ArticleSerializer(article, data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return JsonResponse(serializer.data)
+    #     return JsonResponse(serializer.errors, status=400)
+
+    # elif request.method == 'DELETE':
+    #     article.delete()
+    #     return HttpResponse(status=204)
+
+
+    # Fro API View
     try:
         article = Article.objects.get(pk=pk)
     except Article.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = ArticleSerializer(article)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ArticleSerializer(article, data=data)
+        serializer = ArticleSerializer(article, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
         article.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)
